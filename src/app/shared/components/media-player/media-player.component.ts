08f24339d37f72f3816f9286e0c492114ce9dc8a
Jesus Rojas <jarojas6524@misena.edu.co>
2022-03-20 18:33:58 -0500

    array subscribe and unsubscribe

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
+import { MultimediaService } from '@shared/services/multimedia.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
   styleUrls: ['./media-player.component.css']
 })
-export class MediaPlayerComponent implements OnInit {
+export class MediaPlayerComponent implements OnInit, OnDestroy {
   mockCover: TrackModel = {
     cover: 'https://www.ngenespanol.com/wp-content/uploads/2018/08/La-primera-imagen-de-la-historia.jpg',
     album: 'Angular',
@@ -14,10 +16,22 @@ export class MediaPlayerComponent implements OnInit {
     url: 'http://localhost/track.mp3',
     _id: 1
   }
+  listObservers$: Subscription[] = []
+  state: string = 'paused'
 
-  constructor() { }
+  constructor(private multimediaService: MultimediaService) { }
 
   ngOnInit(): void {
+    const observer1$: Subscription = this.multimediaService.callback
+      .subscribe((res: TrackModel) => {
+        console.log(res)
+      })
+    this.listObservers$ = [observer1$]
   }
 
+  ngOnDestroy(): void {
+    this.listObservers$.forEach( observer => {
+      observer.unsubscribe()
+    })
+  }
 }
