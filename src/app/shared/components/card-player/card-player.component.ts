e6de29e0ebb09b48a386ed74796d396321060fe5
Jesus Rojas <jarojas6524@misena.edu.co>
2022-03-20 16:21:06 -0500

    css card

--- src/app/shared/components/card-player/card-player.component.ts
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-card-player',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-player.component.css']
 })
 export class CardPlayerComponent implements OnInit {
+  @Input() mode: 'small' | 'big' = 'small'
+  @Input() track: TrackModel = { _id: 0, name: '', album: '', url: '', cover: '' };
 
+  // constructor(private multimediaService: MultimediaService) { }
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  sendPlay(track: TrackModel): void {
+    // this.multimediaService.trackInfo$.next(track)
+  }
 }
--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-media-player',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent implements OnInit {
-  mockCover: any = {
+  mockCover: TrackModel = {
     cover: 'https://www.ngenespanol.com/wp-content/uploads/2018/08/La-primera-imagen-de-la-historia.jpg',
     album: 'Angular',
     name: 'Prueba',
+    url: 'http://localhost/track.mp3',
+    _id: 1
   }
 
   constructor() { }
