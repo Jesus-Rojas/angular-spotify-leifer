dbc40a3200f1dd63fbd5e33f7f02a3b29a12edd3
Jesus Rojas <jarojas6524@misena.edu.co>
2022-03-21 02:04:55 -0500

    inciando testing

--- src/app/shared/directives/img-broken.directive.spec.ts
+++ src/app/shared/directives/img-broken.directive.spec.ts
@@ -1,8 +1,10 @@
+import { ElementRef } from '@angular/core';
 import { ImgBrokenDirective } from './img-broken.directive';
 
 describe('ImgBrokenDirective', () => {
   it('should create an instance', () => {
-    const directive = new ImgBrokenDirective();
+    const el = new ElementRef('')
+    const directive = new ImgBrokenDirective(el);
     expect(directive).toBeTruthy();
   });
 });
