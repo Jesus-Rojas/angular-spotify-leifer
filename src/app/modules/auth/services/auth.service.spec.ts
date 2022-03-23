6546995e0f2d4b9c78e2f76af0542b17d1f9e1fd
Jesus Rojas <jarojas6524@misena.edu.co>
2022-03-23 02:19:38 -0500

    testing service

--- src/app/modules/auth/services/auth.service.spec.ts
+++ src/app/modules/auth/services/auth.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { AuthService } from './auth.service';
+import * as mockRaw from '@data/user.json';
+import { of } from 'rxjs';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let mockUser: any = (mockRaw as any).default
+  let httpClientSpy: { post: jasmine.Spy }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -11,10 +15,36 @@ describe('AuthService', () => {
         HttpClientTestingModule
       ]
     });
-    service = TestBed.inject(AuthService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post'])
+    service = new AuthService(httpClientSpy as any)
+    // service = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Debe retornar un objeto con "data" y "tokenSession"', (
+    done: DoneFn
+  ) => {
+    // Arrange
+    const user: any = mockUser.userOk
+    const mockResponse = {
+      data: {},
+      tokenSession: '0-0-0x'
+    }
+
+    httpClientSpy.post.and.returnValue(
+      of(mockResponse)
+    )
+
+    //Act
+    service.sendCredentials(user.email, user.password)
+      .subscribe( res => {
+        const getProperties = Object.keys(res)
+        expect(getProperties).toContain('data');
+        expect(getProperties).toContain('tokenSession');
+        done()
+      })
   });
 });
