030bc863be7301961f4f2e4c6f4c107be1c08ae3
Jesus Rojas <jarojas6524@misena.edu.co>
2022-03-23 01:59:44 -0500

    testing component

--- src/app/modules/auth/pages/login-page/login-page.component.spec.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginPageComponent } from './login-page.component';
@@ -25,7 +26,28 @@ describe('LoginPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  // ? Patro AAA
+  it('Debe retornar invalido', () => {
+    // ! Arrange
+    const mockCredentials = {
+      email: '0x0x0x0x0x0',
+      password: '1111111111111111'
+    }
+    const emailForm: any = component.formLogin.get('email')
+    const passwordForm: any = component.formLogin.get('password')
+
+    // ! Act
+    emailForm.setValue(mockCredentials.email)
+    passwordForm.setValue(mockCredentials.password)
+    // ! Assert
+    expect(component.formLogin.invalid).toEqual(true);
+  });
+
+  it('El boton deberia tener la palabra "Iniciar Sesión"', () => {
+
+    const elementRef = fixture.debugElement.query(By.css('.form-action button'))
+    const getInnerText = elementRef.nativeElement.innerText
+
+    expect(getInnerText).toEqual('Iniciar Sesión');
   });
 });
